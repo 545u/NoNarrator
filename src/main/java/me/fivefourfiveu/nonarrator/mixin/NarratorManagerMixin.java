bde6e80977b70849ac0e92691c3840e6d359436e
545u <47539026+545u@users.noreply.github.com>
2023-01-25 19:04:44 +0200

    Add files via upload

---
+++ src/main/java/me/fivefourfiveu/nonarrator/mixin/NarratorManagerMixin.java
@@ -0,0 +1,23 @@
+package me.fivefourfiveu.nonarrator;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.option.NarratorMode;
+import net.minecraft.client.util.NarratorManager;
+
+@Mixin(NarratorManager.class)
+public class NarratorManagerMixin {
+	@Inject(method = "getNarratorMode", at = @At("HEAD"), cancellable = true)
+	private void getNarrator(CallbackInfoReturnable<NarratorMode> ci) {
+		ci.setReturnValue(NarratorMode.OFF);
+	}
+	
+	@Inject(method = "onModeChange", at = @At("HEAD"), cancellable = true)
+	private void onModeChange(CallbackInfo ci) {
+		ci.cancel();
+	}
+}
